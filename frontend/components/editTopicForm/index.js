e5c57dcf72fdd94feb75d401e85acecef7c62549
sup25 <suparnaa51@gmail.com>
2024-02-02 09:05:55 +0545

    fixed reload and make edit code working

--- frontend/components/editTopicForm/index.js
+++ frontend/components/editTopicForm/index.js
@@ -1,14 +1,42 @@
-import React from "react";
+"use client";
 
-export default function EditTopicForm() {
+import React, { useState } from "react";
+import { useRouter } from "next/navigation";
+
+export default function EditTopicForm({ id, title, description }) {
+  const [newTitle, setNewTitle] = useState(title);
+  const [newDescription, setNewDescription] = useState(description);
+  const router = useRouter();
+  const handleSumbimt = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await fetch(`http://localhost:3000/api/topics/${id}`, {
+        method: "PUT",
+        headers: {
+          "Content-type": "application/json",
+        },
+        body: JSON.stringify({ newTitle, newDescription }),
+      });
+      if (!res.ok) {
+        throw new Error("Failed to update");
+      }
+      router.push("/");
+      router.refresh();
+    } catch (error) {}
+  };
+
   return (
-    <form className="flex flex-col gap-3">
+    <form onSubmit={handleSumbimt} className="flex flex-col gap-3">
       <input
+        onChange={(e) => setNewTitle(e.target.value)}
+        value={newTitle}
         className="border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Topic Title"
       />
       <input
+        onChange={(e) => setNewDescription(e.target.value)}
+        value={newDescription}
         className="border border-slate-500 px-8 py-2"
         type="text"
         placeholder="Topic Description"
--- frontend/components/removeBtn/index.js
+++ frontend/components/removeBtn/index.js
@@ -1,10 +1,8 @@
 "use client";
 
 import { HiOutlineTrash } from "react-icons/hi";
-import { useRouter } from "next/navigation";
 
 export default function RemoveBtn({ id }) {
-  const router = useRouter();
   const removeTopic = async () => {
     const confirmed = confirm("Are you sure?");
 
@@ -14,7 +12,7 @@ export default function RemoveBtn({ id }) {
       });
 
       if (res.ok) {
-        router.refresh();
+        location.reload();
       }
     }
   };
